817f77b608c483139f179feb7efc0306aa13e4ff
ericle.costa <ericle.costa@gmail.com>
2023-07-07 16:48:27 -0400

    alt2

--- projeto_teste/app_teste/views.py
+++ projeto_teste/app_teste/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 import csv
+import json
+from django.http import HttpResponse
 
 # Create your views
 
 def home(request):
     return render(request,'testes/home.html')
+
+def ler_csv(json):
+    csv_fp = open('conexoes.csv', 'r')
+    reader = csv.DictReader(csv_fp,delimiter=';')
+    data = [row for row in reader]
+    return HttpResponse( json.dumps( data ) )
 
 def conexoes(request):
     csv_fp = open('conexoes.csv', 'r')
@@ -23,3 +31,10 @@ def conf_conexao(request):
         if  request.POST.get(reg.get('no_dados')) != None:
             reg_v = reg
     return render(request,'testes/config_con.html',{'data' : reg_v, 'st_altera' : alt_v})
+
+
+def alterar_conexao(request):
+    csv_fp = open('conexoes.csv', 'r')
+    reader = csv.DictReader(csv_fp,delimiter=';')
+    out = [row for row in reader]
+    reg_v = {'no_dados': '', 'host': '', 'database': '', 'port': '', 'user': '', 'password': '', 'st_conexao': ''}
