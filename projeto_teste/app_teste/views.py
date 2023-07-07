71329ff7da5ee21cce39cab3c8f328d96874d847
ericle.costa <ericle.costa@gmail.com>
2023-07-07 06:51:24 -0400

    teste

--- projeto_teste/app_teste/views.py
+++ projeto_teste/app_teste/views.py
@@ -14,5 +14,13 @@ def conexoes(request):
     return render(request,'testes/conexao.html',{'data' : out})
 
 def conf_conexao(request):
-    banco = request.POST.get('')
-    return render(request,'testes/config_con.html')
+    csv_fp = open('conexoes.csv', 'r')
+    reader = csv.DictReader(csv_fp,delimiter=';')
+    out = [row for row in reader]
+    nome_banco = 'teste'
+    #for reg in out:
+        #banco_post = "'" + reg.get("no_dados") + "'"
+        #if  request.POST.get("'" + reg.get("no_dados") + "'") != None:
+            #nome_banco = request.POST.get("'" + reg.get("no_dados") + "'")
+    nome_banco = request.POST.get('sinan')
+    return render(request,'testes/config_con.html',{'banco' : nome_banco})
